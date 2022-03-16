30fe02f53c5487fe71fd1c92ff1dfcdf6f3ee4dc
LampTse <92904636+LampTse@users.noreply.github.com>
2022-03-16 02:28:38 +0200

    changes

--- tabs.js
+++ tabs.js
@@ -1,18 +1,32 @@
 // Tabs
 
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const lists = document.querySelectorAll(".destinations__content ul");
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
+lists.forEach((list) => {
+  const lis = list.querySelectorAll("li");
+  lis.forEach((li) => {
+    li.addEventListener("click", (event) => {
+      // Remove other active class from li's from same parent
+      const parentUl = li.parentElement;
+      const parentLis = parentUl.querySelectorAll("li");
+      parentLis.forEach((li) => {
+        li.classList.remove("active");
+      });
+      // Add active to clicked li
+      li.classList.add("active");
+
+      // Enable active tab content
+      let tabID = event.target.getAttribute("data-tab-target");
+      tabID = tabID.slice(1);
+      const tabContent = parentUl.parentElement.querySelector(".tab-content");
+      const contents = tabContent.querySelectorAll("div");
+      contents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      const content = tabContent.querySelector(`#${tabID}`);
+      content.classList.add("active");
+    });
+  });
+});
+
+
