44fb76f320b58448fce95d587168b8114817ebe4
LampTse <92904636+LampTse@users.noreply.github.com>
2022-03-20 17:28:42 +0200

    changes

--- slider.js
+++ slider.js
@@ -3,11 +3,11 @@ var images = [];
 var time = 2000;
 
 // Image list
-images[0] = '/images/about_us/slider/1.jpg';
-images[1] = '/images/about_us/slider/2.jpg';
-images[2] = '/images/about_us/slider/3.jpg';
-images[3] = '/images/about_us/slider/4.jpg';
-images[3] = '/images/about_us/slider/5.jpg';
+images[0] = 'images/about_us/slider/1.jpg';
+images[1] = 'images/about_us/slider/2.jpg';
+images[2] = 'images/about_us/slider/3.jpg';
+images[3] = 'images/about_us/slider/4.jpg';
+images[3] = 'images/about_us/slider/5.jpg';
 
 // Change Image
 function changeImg() {
